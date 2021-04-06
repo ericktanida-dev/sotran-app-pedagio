cbc22f70b81a1c68cfa18a511ef3598a0735ed1c
erick tanida <67331859+ericktanida-dev@users.noreply.github.com>
2021-04-06 10:47:17 -0300

    a

--- AppPedagioTmovPay/Providers/CardReader/Models/BalanceCardDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AppPedagioTmovPay.Providers.CardReader.Models
-{
-    public class BalanceCardDto
-    {
-        public string Pan { get; set; }
-        public decimal? Balance { get; set; }
-    }
-}
